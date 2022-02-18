c74477ec4d5110f6f5d582d64669b4157e19227d
nian8 <extrafly2000@gmail.com>
2022-02-18 16:35:24 +0800

    resttemplate 调用增加 sentinel 限流示例

---
+++ sentinel/rest-consumer/src/main/java/com/yee/sca/sentinel/consumer/SentinelRestConsumer.java
@@ -0,0 +1,13 @@
+package com.yee.sca.sentinel.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SentinelRestConsumer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SentinelRestConsumer.class, args);
+    }
+
+}
