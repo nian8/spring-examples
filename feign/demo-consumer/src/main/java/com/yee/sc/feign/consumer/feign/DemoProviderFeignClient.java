b825fd1d911a15491020ff7849726c22eef65ae2
nian8 <extrafly2000@gmail.com>
2022-02-18 10:25:16 +0800

    抽象 服务提供者api，方便服务provider/consumer同时依赖

--- feign/demo-consumer/src/main/java/com/yee/sc/feign/consumer/feign/DemoProviderFeignClient.java
+++ feign/demo-consumer/src/main/java/com/yee/sc/feign/consumer/feign/DemoProviderFeignClient.java
@@ -1,13 +1,9 @@
 package com.yee.sc.feign.consumer.feign;
 
+import com.yee.sc.feign.provider.api.ProviderService;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "feign-demo-provider")
-public interface DemoProviderFeignClient {
-
-    @GetMapping("/echo")
-    String echo(@RequestParam("name") String name);
+public interface DemoProviderFeignClient extends ProviderService {
 
 }
--- feign/demo-provider/src/main/java/com/yee/sc/feign/provider/controller/ProviderController.java
+++ feign/demo-provider/src/main/java/com/yee/sc/feign/provider/controller/ProviderController.java
@@ -1,23 +1,27 @@
 package com.yee.sc.feign.provider.controller;
 
+import com.yee.sc.feign.provider.api.ProviderService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class ProviderController {
+public class ProviderController implements ProviderService {
 
     private final Logger logger = LoggerFactory.getLogger(ProviderController.class);
 
     @Value("${server.port}")
     private Integer serverPort;
 
-    @GetMapping("/echo")
-    public String echo(String name) throws InterruptedException {
+    @Override
+    public String echo(String name) {
         // 模拟执行 100ms 时长。方便后续我们测试请求超时
-        Thread.sleep(100L);
+        try {
+            Thread.sleep(100L);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         // 记录被调用的日志
         logger.info("[echo][被调用啦 name({})]", name);
