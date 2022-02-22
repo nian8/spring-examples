e7c78b68fb4d962756a17a03d9de04d2002d8f32
nian8 <extrafly2000@gmail.com>
2022-02-22 15:53:46 +0800

    rabbitmq 批量消费，list 序列化有问题，暂时仍旧用 byte[] 代替

--- stream/rabbitmq/demo-consumer/src/main/java/com/yee/sc/rabbit/consumer/config/RabbitListenerConfig.java
+++ stream/rabbitmq/demo-consumer/src/main/java/com/yee/sc/rabbit/consumer/config/RabbitListenerConfig.java
@@ -3,6 +3,7 @@ package com.yee.sc.rabbit.consumer.config;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.boot.autoconfigure.amqp.SimpleRabbitListenerContainerFactoryConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,13 +13,20 @@ public class RabbitListenerConfig {
 
     @Bean
     public SimpleRabbitListenerContainerFactory rabbitListenerContainerFactory(SimpleRabbitListenerContainerFactoryConfigurer configurer,
-                                                                               ConnectionFactory connectionFactory) {
+                                                                               ConnectionFactory connectionFactory,
+                                                                               MessageConverter messageConverter) {
 
         SimpleRabbitListenerContainerFactory factory = new SimpleRabbitListenerContainerFactory();
-        // 设置消息转换器
-        factory.setMessageConverter(new Jackson2JsonMessageConverter());
-        //
         configurer.configure(factory, connectionFactory);
+        // 设置消息转换器
+        factory.setMessageConverter(messageConverter);
         return factory;
+    }
+
+    @Bean
+    MessageConverter messageConverter() {
+        Jackson2JsonMessageConverter converter = new Jackson2JsonMessageConverter();
+        converter.setAlwaysConvertToInferredType(true);
+        return converter;
     }
 }
