21374943ca19f4bc0141a360cd789bdce98f2566
nian8 <extrafly2000@gmail.com>
2022-03-02 10:48:23 +0800

    只保留websocket client 示例

--- websocket/websocket-client/src/main/java/com/yee/spring/ws/task/MessageTask.java
+++ websocket/websocket-client/src/main/java/com/yee/spring/ws/task/MessageTask.java
@@ -18,7 +18,7 @@ public class MessageTask {
     WebsocketService websocketService;
 
     @Async
-    @Scheduled(cron = "0/1 * * * * ?")
+    @Scheduled(cron = "0/15 * * * * ?")
     public void run() {
         //
         SendReq sendReq = new SendReq();
